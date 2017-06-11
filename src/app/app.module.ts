2553354a679817a38c3af967ccd26bbc30fd08e0
Uğur Korfalı <ugurkorfali@KLMC07787.local>
2017-06-11 16:13:28 +0200

    add Apollo module and configure it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:3000/raspberry-api'
+  })
+});
+
+export function provideClient(): ApolloClient {
+  return client;
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApolloModule.forRoot(provideClient)
   ],
   providers: [],
   bootstrap: [AppComponent]
