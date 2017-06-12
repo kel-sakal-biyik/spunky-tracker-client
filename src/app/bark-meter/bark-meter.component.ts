06ff1464e7bc5379d9dc73da875b8e40cd8361d3
Uğur Korfalı <ugurkorfali@KLMC07787.local>
2017-06-12 23:42:02 +0200

    add bark meter functionality

--- src/app/bark-meter/bark-meter.component.ts
+++ src/app/bark-meter/bark-meter.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const GetDecibel = gql`
+  query GetDecibel {
+    microphone {
+      decibel
+    }
+  }
+`;
+
+const IncreasePlayCount = gql`
+  mutation IncreaseNoBarkingCount($playType: String!) {
+    countPlay(playType: $playType) {
+      value
+    }
+  }
+`;
 
 @Component({
   selector: 'app-bark-meter',
   templateUrl: './bark-meter.component.html',
   styleUrls: ['./bark-meter.component.css']
 })
-export class BarkMeterComponent {}
+export class BarkMeterComponent implements OnInit{
+  public decibel: number;
+  public playCount: number = 0;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo.watchQuery<any>({
+      query: GetDecibel,
+      pollInterval: 10000
+    }).subscribe(({data}) => {
+      this.decibel = data.microphone.decibel;
+    });
+  }
+
+  increaseCount() {
+    this.apollo.mutate<any>({
+      mutation: IncreasePlayCount,
+      variables: {
+        playType: 'noBarking'
+      }
+    }).subscribe(({ data }) => {
+      this.playCount = data.countPlay.value;
+    },(error) => {
+      console.log('there was an error sending the query', error);
+    });
+  }
+}
